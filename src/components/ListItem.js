dd06bb2222e6b183fe8f21a6ca6629e1f0d719c2
matthias haefeli <haefeli.matthias@gmail.com>
2020-09-03 15:37:31 -0600

    animation

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Text, TouchableWithoutFeedback, View, LayoutAnimation } from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
-  renderDescription() {
-    const { library, selectedLibraryId } = this.props
+  componentDidUpdate() {
+    LayoutAnimation.spring();
+  }
 
-    if (library.item.id === selectedLibraryId) {
+  renderDescription() {
+    const { library, expanded } = this.props
+
+    if (expanded) {
       return (
-        <Text>{library.item.description}</Text>
+        <CardSection>
+          <Text style={{ flex: 1}}>
+            {library.item.description}
+          </Text>
+        </CardSection>
       );
     }
   }
@@ -43,8 +51,10 @@ const styles = {
   }
 };
 
-const mapStateToProps = state => {
-  return { selectedLibraryId: state.selectedLibraryId };
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.item.id
+
+  return { expanded };
 }
 
 export default connect(mapStateToProps, actions)(ListItem);

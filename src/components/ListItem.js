2ef2513f4d4a1ba3edc7bab81d761d3ee28d8fe0
matthias haefeli <haefeli.matthias@gmail.com>
2020-09-03 15:13:29 -0600

    show desc

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,6 +5,16 @@ import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  renderDescription() {
+    const { library, selectedLibraryId } = this.props
+
+    if (library.item.id === selectedLibraryId) {
+      return (
+        <Text>{library.item.description}</Text>
+      );
+    }
+  }
+
   render() {
     const { titleStyle } = styles;
     const { id, title } = this.props.library.item;
@@ -19,6 +29,7 @@ class ListItem extends Component {
               {title}
             </Text>
           </CardSection>
+          {this.renderDescription()}
           </View>
       </TouchableWithoutFeedback>
     );
@@ -32,4 +43,8 @@ const styles = {
   }
 };
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = state => {
+  return { selectedLibraryId: state.selectedLibraryId };
+}
+
+export default connect(mapStateToProps, actions)(ListItem);
